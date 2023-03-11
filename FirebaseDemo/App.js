9c27969ab672a5fc49b0741a748252e4c7e9a46a
ProExeTest2 <127487107+ProExeTest2@users.noreply.github.com>
2023-03-11 10:03:46 +0530

    demo

--- FirebaseDemo/App.js
+++ FirebaseDemo/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import MainNavigation from './src/navigation/MainNavigation';
+import React from "react";
+import MainNavigation from "./src/navigation/MainNavigation";
 
 const App = () => {
   return <MainNavigation />;
 };
 
-export default App;//hello
-
+export default App; //hello
+//this is demo
